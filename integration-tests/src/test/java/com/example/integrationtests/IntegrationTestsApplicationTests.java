d56ea4ea6b08f37155a45c5a861bb8abf67c9d9e
Ihor Rybak <jrybak23@gmail.com>
2022-03-27 11:32:26 +0300

    parse csv content to assert integration tests results

--- integration-tests/src/test/java/com/example/integrationtests/IntegrationTestsApplicationTests.java
+++ integration-tests/src/test/java/com/example/integrationtests/IntegrationTestsApplicationTests.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.example.integrationtests.TestUtil.readClasspathFileContent;
 import static com.example.integrationtests.TestUtil.readFileContent;
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class IntegrationTestsApplicationTests {
@@ -20,7 +24,23 @@ class IntegrationTestsApplicationTests {
     void assertKnowDatabaseResult(String database) {
         String actualFileContent = readFileContent(RESULTS_OUTPUT_PATH + database + ".csv");
         String expectedFileContent = readClasspathFileContent("results/"  + database + ".csv");
-        assertEquals(expectedFileContent, actualFileContent);
+        List<List<String>> actualContentTable = parseTable(actualFileContent);
+        List<List<String>> expectedContentTable = parseTable(expectedFileContent);
+        if (!expectedContentTable.equals(actualContentTable)) {
+            assertEquals(expectedFileContent, actualFileContent);
+        }
+    }
+
+    private List<List<String>> parseTable(String csvFileContent) {
+        return csvFileContent.lines()
+                .map(this::parseCells)
+                .collect(toList());
+    }
+
+    private List<String> parseCells(String line) {
+       return Arrays.stream(line.split(","))
+               .map(String::trim)
+               .collect(toList());
     }
 
 }
